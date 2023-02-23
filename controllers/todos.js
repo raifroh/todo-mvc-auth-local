b78902bfa6feb85b2f1caae5b090addf66952063
Rai_Froh <raifroh@gmail.com>
2023-02-22 16:20:23 -0800

    added the tasks completed counter

--- controllers/todos.js
+++ controllers/todos.js
@@ -6,7 +6,8 @@ module.exports = {
         try{
             const todoItems = await Todo.find({userId:req.user.id})
             const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false})
-            res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user: req.user})
+            const itemsCompleted = await Todo.countDocuments({userId:req.user.id,completed: true})
+            res.render('todos.ejs', {todos: todoItems, left: itemsLeft, done: itemsCompleted, user: req.user})
         }catch(err){
             console.log(err)
         }
