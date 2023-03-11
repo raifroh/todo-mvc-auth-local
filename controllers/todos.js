65ad0bc8682313ea6a999ae433a3b7f13ba1f28b
drew <drewbeland@gmail.com>
2023-03-10 18:25:08 -0800

    use datalist in views to have drop down of previously created todo categories

--- controllers/todos.js
+++ controllers/todos.js
@@ -7,14 +7,21 @@ module.exports = {
             const todoItems = await Todo.find({userId:req.user.id})
             const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false})
             const itemsCompleted = await Todo.countDocuments({userId:req.user.id,completed: true})
-            res.render('todos.ejs', {todos: todoItems, left: itemsLeft, done: itemsCompleted, user: req.user})
+            //find all the docs made by the user and get the category key value pair
+            const allCategories = await Todo.find({userId:req.user.id}, "category")
+            //put the values of the category property into an array
+            const categoryArray = await allCategories.map(doc => doc['category'])
+            //get rid of duplicate values and put it into an array
+            const uniqueCategories = await [...new Set(categoryArray)]
+            console.log(uniqueCategories)
+            res.render('todos.ejs', {todos: todoItems, left: itemsLeft, done: itemsCompleted, user: req.user, categoryOptions: uniqueCategories})
         }catch(err){
             console.log(err)
         }
     },
     createTodo: async (req, res)=>{
         try{
-            await Todo.create({todo: req.body.todoItem, completed: false, userId: req.user.id, category: req.body.category.toLowerCase()})
+            await Todo.create({todo: req.body.todoItem, completed: false, userId: req.user.id, category: req.body.category})
             console.log('Todo has been added!')
             res.redirect('/todos')
         }catch(err){
